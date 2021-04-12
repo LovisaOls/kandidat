c622f4dbacfe706109f8b4dab1f3eb9b79818ad4
sofiehulte <sofie.hulteberg.4390@student.uu.se>
2021-04-12 16:32:35 +0200

    hej

--- App.js
+++ App.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import {  } from 'react-native';
 
-import MyProfileScreen from '../kandidat/Screens/MyProfileScreen';
+//import MyProfileScreen from '../kandidat/Screens/MyProfileScreen';
 import WelcomeScreen from './Screens/WelcomeScreen';
-import RegistrationScreen from './Screens/RegistrationScreen';
-import { NavigationContainer } from '@react-navigation/native'
+//import RegistrationScreen from './Screens/RegistrationScreen';
+//import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import config from "./Firebase/config";
 import firebase from "firebase/app";
