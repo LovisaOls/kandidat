3fc6dfd72ff3a2bf6d6ac602440aacd798e28a59
LovisaOls <lovisa.olsson.4242@student.uu.se>
2021-05-04 10:23:09 +0200

    lite saker som inte funkar :)

--- Screens/LoadingScreen.js
+++ Screens/LoadingScreen.js
@@ -6,7 +6,7 @@ import { Image, StyleSheet, View, Dimensions } from "react-native";
 import { useDispatch } from "react-redux";
 
 import { Actions } from "react-native-router-flux";
-import { setCurrentUser, fetchUserTeams } from "../actions/index";
+import { setCurrentUser } from "../actions/index";
 
 function LoadingScreen() {
   const dispatch = useDispatch();
@@ -14,12 +14,8 @@ function LoadingScreen() {
     firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
         // User is signed in.
-        console.log("user is already signed in");
-        dispatch(fetchUserTeams(user.uid));
-        //Väntar 2 sekunder
-        setTimeout(() => {
-          dispatch(setCurrentUser(user.uid));
-        }, 2000);
+        console.log("user is signed in");
+        dispatch(setCurrentUser(user.uid));
       } else {
         // No user is signed in.
         console.log("no user --> sign in");
@@ -42,6 +38,7 @@ const screenWidth = Dimensions.get("window").width;
 const styles = StyleSheet.create({
   image: {
     width: screenWidth * 0.8,
+    height: "auto",
   },
   theLogo: {
     alignItems: "center",
